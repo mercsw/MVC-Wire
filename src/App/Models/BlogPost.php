c14f19aa286c23dd2fe4014bcf6f886baf397366
Michel Chamberland <merc@securitywire.com>
2012-03-03 01:11:39 -0500

    more comments

--- src/App/Models/BlogPost.php
+++ src/App/Models/BlogPost.php
@@ -2,6 +2,7 @@
 
 class BlogPost extends Model {
 	
+	//function name must match property name in all lowercase with the string "Validator" appended to it
 	function postdateValidator($field, $value)
 	{
 		Validators::MatchesRegex($field, $value, '/^\d{10}$/', "Field $field must be 10 digits");
